24ac037b241751f608a2173b64c9eadd69a98c4a
Isabel Kraft <isabel.kraft@student.kit.edu>
2023-01-06 14:07:18 +0100

    add basic headphone connection code

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,101 @@ class _StartPageState extends State<StartPage> {
     );
   }
 
+  // headphone part
+
+  String _connectionStatus = "Disconnected";
+
+  String _accX = "-";
+  String _accY = "-";
+  String _accZ = "-";
+
+  bool _isConnected = false;
+
+  bool earConnectFound = false;
+
+
+
+
+  void updateAccelerometer(rawData) {
+    Int8List bytes = Int8List.fromList(rawData);
+
+    // description based on placing the earable into your right ear canal
+    int acc_x = bytes[14];
+    int acc_y = bytes[16];
+    int acc_z = bytes[18];
+
+    setState(() {
+      _accX = acc_x.toString() + " (unknown unit)";
+      _accY = acc_y.toString() + " (unknown unit)";
+      _accZ = acc_z.toString() + " (unknown unit)";
+    });
+  }
+
+  int twosComplimentOfNegativeMantissa(int mantissa) {
+    if ((4194304 & mantissa) != 0) {
+      return (((mantissa ^ -1) & 16777215) + 1) * -1;
+    }
+
+    return mantissa;
+  }
+
+  void _connect() {
+    FlutterBlue flutterBlue = FlutterBlue.instance;
+
+    // start scanning
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+    // listen to scan results
+    var subscription = flutterBlue.scanResults.listen((results) async {
+
+      // do something with scan results
+      for (ScanResult r in results) {
+        if (r.device.name == "earconnect" && !earConnectFound) {
+          earConnectFound = true; // avoid multiple connects attempts to same device
+
+          await flutterBlue.stopScan();
+
+          r.device.state.listen((state) { // listen for connection state changes
+            setState(() {
+              _isConnected = state == BluetoothDeviceState.connected;
+              _connectionStatus = (_isConnected) ? "Connected" : "Disconnected";
+            });
+          });
+
+          await r.device.connect();
+
+          var services = await r.device.discoverServices();
+
+          for (var service in services) { // iterate over services
+            for (var characteristic in service.characteristics) { // iterate over characterstics
+              switch (characteristic.uuid.toString()) {
+                case "0000a001-1212-efde-1523-785feabcd123":
+                  print("Starting sampling ...");
+                  await characteristic.write([0x32, 0x31, 0x39, 0x32, 0x37, 0x34, 0x31, 0x30, 0x35, 0x39, 0x35, 0x35, 0x30, 0x32, 0x34, 0x35]);
+                  await Future.delayed(new Duration(seconds: 2)); // short delay before next bluetooth operation otherwise BLE crashes
+                  characteristic.value.listen((rawData) => {
+                    updateAccelerometer(rawData),
+
+                  });
+                  await characteristic.setNotifyValue(true);
+                  await Future.delayed(new Duration(seconds: 2));
+                  break;
+
+
+
+                case "00002a1c-0000-1000-8000-00805f9b34fb":
+                  await characteristic.setNotifyValue(true);
+                  await Future.delayed(new Duration(seconds: 2)); // short delay before next bluetooth operation otherwise BLE crashes
+                  break;
+              }
+            };
+          };
+        }
+
+      }
+    }
+    );
+  }
 
 
 }
